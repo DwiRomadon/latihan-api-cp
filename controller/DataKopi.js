f9e6e7482cc6c364559c1c746bfa23cc414c6a2e
DwiRomadon <dwi.13421054@student.ubl.ac.id>
2023-05-20 10:05:02 +0700

    crud

--- controller/DataKopi.js
+++ controller/DataKopi.js
@@ -19,3 +19,96 @@ exports.create = async (req, res) => {
     }
 }
 
+exports.findAll = async (req, res) => {
+    try {
+        const query = await prisma.data_kopi.findMany()
+
+        if (query.length > 0) {
+            res.json({
+                status: true,
+                msg: 'Berhasil memuat',
+                result: query
+            })
+        } else {
+            res.json({
+                status: false,
+                msg: "Tidak ada data jenis kopi",
+                result: query
+            })
+        }
+    } catch (error) {
+        res.json({
+            status: false,
+            msg: "Terjadi kesalahan pada server"
+        })
+    }
+}
+
+exports.findById = async (req, res) => {
+    try {
+        const query = await prisma.data_kopi.findUnique({
+            where: {
+                id: Number(req.params.id)
+            }
+        })
+
+        if (query) {
+            res.json({
+                status: true,
+                msg: "Berhasil memuat",
+                result: query
+            })
+        } else {
+            res.json({
+                status: false,
+                msg: `Tidak ada data untuk ID => ${req.params.id} !!!`
+            })
+        }
+    } catch (error) {
+        res.json({
+            status: false,
+            msg: `Terjadi kesalahan pada server !!!`
+        })
+    }
+}
+
+exports.updateData = async (req, res) => {
+    try {
+        const update = await prisma.data_kopi.update({
+            where: {
+                id: Number(req.params.id)
+            },
+            data: req.body
+        })
+
+        res.json({
+            status: true,
+            msg: 'Berhasil update data!!'
+        })
+
+    } catch (error) {
+        res.json({
+            status: false,
+            msg: `Terjadi kesalahan pada server !!!`
+        })
+    }
+}
+
+exports.deleteData = async (req, res) => {
+    try {
+        const hapus = await prisma.data_kopi.delete({
+            where: {
+                id: Number(req.params.id)
+            }
+        })
+        res.json({
+            status: true,
+            msg: 'Berhasil menghapus data!!'
+        })
+    } catch (error) {
+        res.json({
+            status: false,
+            msg: "Terjadi kesalahan pada server!!"
+        })
+    }
+}
--- routes/DataKopi.js
+++ routes/DataKopi.js
@@ -2,5 +2,7 @@ const router = require('express').Router()
 const controller = require('../controller/DataKopi')
 
 router.post('/create', controller.create)
+router.get('/get-data', controller.findAll)
+router.get('/get-data/:id', controller.findById)
 
 module.exports = router
